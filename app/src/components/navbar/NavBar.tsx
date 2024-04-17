34980d48d2c7b1fc779bbf09835be22bc54d5fef
flakesbourg <alexanderfalkenberg9@gmail.com>
2024-04-17 20:37:10 +0200

    burger menu added

--- app/src/components/navbar/NavBar.tsx
+++ app/src/components/navbar/NavBar.tsx
@@ -2,11 +2,14 @@ import { useContext, useEffect, useState } from "react";
 import { ThemeContext } from "../../App";
 import "./NavBar.css";
 import ThemeButton from "./ThemeButton";
+import { CgMenu } from "react-icons/cg";
+import { CgClose } from "react-icons/cg";
 
 function NavBar() {
     const { theme } = useContext(ThemeContext);
     const [projectsVisible, setProjectsVisible] = useState(false);
     const [aboutMeVisible, setAboutMeVisible] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -44,17 +47,42 @@ function NavBar() {
         }
     }
 
+    function toggleMenuOpen() {
+        setMenuOpen(!menuOpen);
+    }
+
     return (
+        <>
         <div className="navBar" style={{ backgroundColor: theme.navbarColor, color: theme.navbarTextColor }}>
             <div className="navHeadline">Alexander Falkenberg</div>
-            <div className="navLinks">
-                <a className={aboutMeVisible ? "link active" : "link"} onClick={() => scrollTo("aboutMe")}>About Me</a>
-                <a className={projectsVisible ? "link active" : "link"} onClick={() => scrollTo("projects")}>Projects</a>
-                <a>Skills</a>
-                <a>Contact</a>
+            <div className="linksContainer">
+                <div className="navLinks">
+                    <a className={aboutMeVisible ? "link active" : "link"} onClick={() => scrollTo("aboutMe")}>About Me</a>
+                    <a className={projectsVisible ? "link active" : "link"} onClick={() => scrollTo("projects")}>Projects</a>
+                    <a>Skills</a>
+                    <a>Contact</a>
+                </div>
+                <ThemeButton />
             </div>
+            <div className="burgerIcon" onClick={toggleMenuOpen}>
+                <CgMenu size={40}/>
+            </div>
+        </div>
+        <div className={menuOpen ? "burgerContainer open" : "burgerContainer"} style={{ backgroundColor: theme.navbarColor, color: theme.navbarTextColor }}>
+        <div className="burgerLinks">
+            <div className="burgerIcon" onClick={toggleMenuOpen}>
+                <CgClose size={40}/>
+            </div>
+            <a className={aboutMeVisible ? "link active" : "link"} onClick={() => scrollTo("aboutMe")}>About Me</a>
+            <a className={projectsVisible ? "link active" : "link"} onClick={() => scrollTo("projects")}>Projects</a>
+            <a>Skills</a>
+            <a>Contact</a>
+        </div>
+        <div className="burgerTheme">
             <ThemeButton />
         </div>
+        </div>
+        </>
     );
 }
 
